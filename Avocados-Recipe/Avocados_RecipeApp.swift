1d9c2ef64b95226331faa9736a4e833e389bd446
smitkumar.khokhariya <smitkumar.khokhariya@corsameets.com>
2024-03-05 21:06:26 -0500

    Initial Commit

---
+++ Avocados-Recipe/Avocados_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avocados_RecipeApp.swift
+//  Avocados-Recipe
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-05.
+//
+
+import SwiftUI
+
+@main
+struct Avocados_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocados-Recipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avocados-Recipe
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
