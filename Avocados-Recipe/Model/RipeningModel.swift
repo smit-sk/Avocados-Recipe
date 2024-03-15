3e91e5c3a7e0dfa0973496d1492edc44873eb9b7
smitkumar.khokhariya <smitkumar.khokhariya@corsameets.com>
2024-03-15 11:51:10 -0400

    RipeningView-model-data-view-stages

---
+++ Avocados-Recipe/Model/RipeningModel.swift
@@ -0,0 +1,18 @@
+//
+//  RipeningModel.swift
+//  Avocados-Recipe
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-13.
+//
+
+import SwiftUI
+
+struct RipeningModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RipeningModel()
+}
---
+++ Avocados-Recipe/View/RipeningView.swift
@@ -0,0 +1,18 @@
+//
+//  RipeningView.swift
+//  Avocados-Recipe
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-11.
+//
+
+import SwiftUI
+
+struct RipeningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RipeningView()
+}
